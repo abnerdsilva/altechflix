bb2b48085a848e14bcbaccf0f3266b34ff657c0d
Abner Silva <abner.computadores@outlook.com>
2020-07-29 16:23:05 -0300

    add paginas de cadastro de categoria e videos e add uso de rotas

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import Logo from '../../assets/img/Logo.png'
 import './Menu.css'
 // import ButtonLink from './ButtonLink'
@@ -7,13 +8,13 @@ import Button from '../Button'
 function Menu() {
     return (
         <nav className="Menu">
-            <a href="/">
+            <Link to="/">
                 <img className="Logo" src={Logo} alt="AluraFlix" />
-            </a>
+            </Link>
 
-            {/* <ButtonLink className="ButtonLink" href="/home">Novo video</ButtonLink> */}
+            {/* <ButtonLink className="ButtonLink" to="/home">Novo video</ButtonLink> */}
 
-            <Button as="a" className="ButtonLink" href="/home">Novo Video</Button>
+            <Button as={Link} className="ButtonLink" to="/cadastro/video">Novo Video</Button>
         </nav>
     )
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/home/App.js';
+
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import Pagina404 from './pages/pagina404'
+import CadastroVideo from './pages/cadastro/video';
+import CadastroCategoria from './pages/cadastro/categoria';
+// import 
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact />
+      <Route path="/cadastro/video" component={CadastroVideo} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} exact />
+      <Route component={Pagina404} />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
-
